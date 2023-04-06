f05fbcf6ba645769fd79804176e772fc705510f5
Diego Ramirez <diegoramirezb67@gmail.com>
2023-04-05 20:10:19 -0500

    Update main.js

--- main.js
+++ main.js
@@ -3,16 +3,24 @@ let reset = document.querySelector(".links__reset")
 let increase = document.querySelector(".links__increase")
 let num = document.querySelector("p")
 
+
 decrease.addEventListener('click', () => {
   num.innerText = parseInt(num.innerText) - 1;
+  color()
 })
 
 reset.addEventListener('click', () => {
   num.innerText = 0;
-  
+  color()
 })
 
 increase.addEventListener('click', () => {
   num.innerText = parseInt(num.innerText) + 1;
-  
+  color()
 })
+
+function color() {
+  if (parseInt(num.innerText) > 0) num.style.color = "green"
+  else if(parseInt(num.innerText) < 0) num.style.color = "red"
+  else num.style.color = "#eee"
+}
